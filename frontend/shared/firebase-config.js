02c57953a53b70fe313a83feb38b526ac56b0c96
laya-resort-hotel <layaresorthotel@gmail.com>
2026-04-14 22:34:14 +0700

    Add files via upload

--- frontend/shared/firebase-config.js
+++ frontend/shared/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseWebConfig = {
-  // ใส่ค่า Firebase เดียวกับระบบ Check-in/IHN ที่ใช้อยู่จริง
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: "AIzaSyDReH8ZE8BLdSSEKoxAL4nppZOd6kO00rA",
+  authDomain: "service-c93f2.firebaseapp.com",
+  projectId: "service-c93f2",
+  storageBucket: "service-c93f2.firebasestorage.app",
+  messagingSenderId: "1011741219243",
+  appId: "1:1011741219243:web:0bbaa94adcb97723efdaee",
+  measurementId: "G-L0X403JZ5N",
 };
